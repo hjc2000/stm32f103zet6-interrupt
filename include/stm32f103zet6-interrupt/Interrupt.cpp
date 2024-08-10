03f678a5f1fd1494bd9dcbb9c0da2c7562ad855c
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-10 21:29:47 +0800

    HAL_NVIC_SetPriorityGrouping(static_cast<uint32_t>(group));

--- include/stm32f103zet6-interrupt/Interrupt.cpp
+++ include/stm32f103zet6-interrupt/Interrupt.cpp
@@ -6,5 +6,5 @@ using namespace hal;
 
 void hal::Interrupt::SetPriorityGroup(PreemptionPriorityGroup group)
 {
-	HAL_NVIC_SetPriorityGrouping((uint32_t)group);
+	HAL_NVIC_SetPriorityGrouping(static_cast<uint32_t>(group));
 }
